25261dc455e21143ba72039f7234c0ea6bbdebd4
ShadowNightCoder <barbar0608@gmail.com>
2024-02-08 14:50:57 +0200

    update the project with some basic knolage

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { FormsModule } from '@angular/forms';
+import { ArticaleComponent } from './components/articale/articale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ArticaleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/src/app/components/articale/articale.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArticaleComponent } from './articale.component';
+
+describe('ArticaleComponent', () => {
+  let component: ArticaleComponent;
+  let fixture: ComponentFixture<ArticaleComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ArticaleComponent]
+    });
+    fixture = TestBed.createComponent(ArticaleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
